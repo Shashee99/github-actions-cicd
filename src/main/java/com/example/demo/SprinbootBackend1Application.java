cd0727160520f528feb3ae714e25f2fc7a44d29e
Shashee99 <2020cs216@stu.ucsc.cmb.ac.lk>
2023-06-30 20:16:46 +0530

    first commit

---
+++ src/main/java/com/example/demo/SprinbootBackend1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinbootBackend1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprinbootBackend1Application.class, args);
+	}
+
+}
